5cc1fe1a42718d1d1b1c580133ba4d7c392f4c28
Роман Светлов <kujivunia@gmail.com>
2022-09-23 17:26:56 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace markov_generator_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.Generic.Dictionary<int, int> d = new System.Collections.Generic.Dictionary<int, int>();
+            d.Add(1, 10);
+            d.Add(2, 12);
+            d.Add(3, 13);
+
+            Console.WriteLine(d);
+            Console.WriteLine(d.Values);
+            Console.WriteLine(d.Values.Sum());
+        }
+    }
+}
